199990461765235a81d8ce8834c3de22afe5f7c2
TerrorBite <terrorbite@lethargiclion.net>
2018-12-11 21:39:03 +1100

    Auth state in own class

--- Auth.py
+++ Auth.py
@@ -7,6 +7,7 @@ from twisted.conch.ssh import service, transport, userauth
 
 # Python imports
 import struct
+from base64 import b64encode
 
 # Our imports
 from Util import log
@@ -47,8 +48,29 @@ class DebugSSHService(service.SSHService):
             ) )
         service.SSHService.packetReceived(self, messageNum, packet)
 
+class UserAuthState(object):
+    def __init__(self, auth, username, service):
+        self.auth = auth
+        self.pubkeys = []
+        self.username = username
+        self.desired_service = service
+        # the following line is terrifying
+        self.user_is_known = self.auth.transport.factory.portal.realm.world.db.user_exists(username)
+        # Stores phase of interactive auth
+        self.phase = 0
 
-class UserAuthService(DebugSSHService):
+    def is_invalid(self, username, service):
+        """
+        Returns True if the supplied username and service name
+        don't match stored values, which means this state is invalid.
+        """
+        return not all((username == self.username,
+                service == self.desired_service))
+
+    def add_key(self, blob):
+        self.pubkeys.append(blob)
+
+class UserAuthService(service.SSHService):
     """
     The UserAuthService replaces the standard SSH authentication
     service. This service customises the login experience in the
@@ -78,13 +100,11 @@ class UserAuthService(DebugSSHService):
 
     def serviceStarted(self):
         log.info("{0} service starting".format(self.name) )
-        self.user = None
-        self.nextService = None
+        self.state = None
 
-        # Stores the user's public key, if they provided one.
-        self.pubKey = None
+        # Stores the user's public keys, if they provided any.
+        self.seen_keys = []
 
-        self.phase = 0
 
     def ssh_USERAUTH_REQUEST(self, packet):
         """
@@ -97,12 +117,12 @@ class UserAuthService(DebugSSHService):
         @type packet: L{bytes}
         """
         user, nextService, method, rest = getNS(packet, 3)
-        if user != self.user or nextService != self.nextService:
-            self.authenticatedWith = [] # clear auth state
-            log.debug(dir(self.transport.factory.portal))
-            self.new_user = self.transport.factory.portal.realm.world.db.user_exists(user)
-        self.user = user
-        self.nextService = nextService
+        if self.state is None or self.state.is_invalid( user, nextService ):
+            # If username or desired service has changed during auth,
+            # the RFC says we must discard all state.
+            self.state = UserAuthState( self, user, nextService )
+            #log.debug(dir(self.transport.factory.portal))
+            self.firstContact()
 
         log.debug( "Auth request for user {0}, service {1}, method {2}.".format(user, nextService, method) )
 
@@ -110,7 +130,7 @@ class UserAuthService(DebugSSHService):
             # If user is known, try and do pubkey auth.
             # If user is not known, store their pubkeys so they can
             #   use one to register with us.
-            #self.auth_pubkey( rest )
+            self.auth_pubkey( rest )
             log.debug(  "Pubkey attempt")
             #return self.continueNextAuth(self.supportedAuthentications)
             return self.failAuth(self.supportedAuthentications)
@@ -130,10 +150,29 @@ class UserAuthService(DebugSSHService):
         """
         Called the first time a user sends us a userauth request.
         """
+        log.debug("First contact from {0}!".format(self.state.username))
+
+    def auth_pubkey(self, pubkey):
+        log.debug("Got a pubkey")
+        log.debug(repr(pubkey))
+        initial = bool(pubkey[0])
+        algo, blob, rest = getNS(pubkey[1:], 2)
+        self.seen_keys.append(blob)
+        log.debug( self.key2str(algo, blob) )
+
+    def store_pubkey(self, pubkey):
+        algo, blob, rest = getNS(pubkey[1:], 2)
+        self.state.add_key(blob)
+        log.debug( self.key2str(algo, blob) )
+        # Tell client that this key didn't auth them
+        self.failAuth(self.supportedAuthentications)
+
+    def key2str(self, algo, blob):
+        return "{0} {1}".format(algo, b64encode(blob))
 
     def tryKeyboardInteractive(self, packet):
         self.askQuestions([
-            ("I haven't seen you before. Do you want to register (yes/no)? ", False)
+            ("\033[1mWelcome, \033[34m{0}\033[39m!\033[0m I don't recognise your username.\nWould you like to \033[4mr\033[0megister, proceed as a \033[4mg\033[0muest, or \033[4mq\033[0muit?\n(r/g/q): ".format(self.state.username), False)
         ])
 
     def ssh_USERAUTH_INFO_RESPONSE(self, packet):
@@ -155,12 +194,20 @@ class UserAuthService(DebugSSHService):
             #d.errback(failure.Failure())
             pass
         else:
-            if self.phase == 0:
-                if( resp[0].lower() == "yes" ):
-                    self.phase += 1
+            if self.state.phase == 0:
+                r = resp[0][0].lower()
+                if( r == "r" ):
+                    self.state.phase += 1
                     self.askQuestions([
                         ("Please choose a password: ", True),
                         ("Please re-enter your password: ", True)
+                    ])
+                elif r == "q":
+                    self.noAuthLeft("Please come back soon!")
+            elif self.state.phase == 1:
+                self.state.phase += 1
+                self.askQuestions([
+                    ("Choose a name for your first character: ", False)
                     ])
             else:
                 #d.callback(resp)
